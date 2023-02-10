85a33e039b2129d3cd06d86299fe3404823a1f07
Ezgi Tokgoz <ezgitokgoz16@gmail.com>
2023-02-10 18:34:04 +0300

    navbar design
    navbar design finished slider creation started

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Container = styled.div`
+    height: 30px;
+    background-color: #3e94df;
+    color:white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight: 500;
+`
+
+const Announcement = () => {
+  return (
+    <Container>
+      Free shipping on your first purchase and 15% off your first purchase!    
+    </Container>
+  )
+}
+
+export default Announcement
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 //import SearchIcon from '@mui/icons-material/Search';
-import { Search } from '@mui/icons-material';
-
-import React from 'react'
-import styled from 'styled-components'
+import { Search, ShoppingCartOutlined } from '@mui/icons-material';
+import { Badge } from '@mui/material';
+//import {Badge} from '@mui/core-material';
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
    height: 60px;
@@ -26,13 +27,37 @@ const Left =styled.div`
     display:flex;
     align-items: center;
 `
-const Center =styled.div`flex: 1;`
-const Right =styled.div`flex: 1;`
+const Center =styled.div`
+    flex: 1;
+    text-align: center;
+`
+const Right =styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    
+`
 
 const SearchContainer=styled.div`
-    border: 1px solid lightgray;
+    border: 0.5px solid lightgray;
     display:flex;
     align-items: center;
+    margin-left:25px;
+    padding:5px;
+`
+
+const Input = styled.input`
+    border:none;
+`
+const Logo = styled.h1`
+    font-weight: bold;
+
+`
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
 `
 
 
@@ -43,12 +68,20 @@ const navbar = () => {
             <Left>
                 <Language>EN</Language>
                 <SearchContainer>
-                    search babyyy
-                    <Search/>
+                    <Input/>
+                    <Search style={{color:"gray",fontSize:16}}/>
                 </SearchContainer>
             </Left>
-            <Center>center</Center>
-            <Right>right</Right>
+            <Center><Logo>.LIVA.</Logo></Center>
+            <Right>
+                <MenuItem>REGISTER</MenuItem>
+                <MenuItem>SIGN IN</MenuItem>
+                <MenuItem>
+                    <Badge badgeContent={4} color="primary" >
+                        <ShoppingCartOutlined />
+                    </Badge>
+                </MenuItem>
+            </Right>
         </Wrapper>
     </Container>
   )
