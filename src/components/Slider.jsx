5fa2a07cd79f985dc6ce1648e729a99489ae0bb9
Ezgi Tokgoz <ezgitokgoz16@gmail.com>
2023-02-14 17:34:43 +0300

    Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,25 +6,94 @@ const Container = styled.div`
     width:100%;
     height:100vh;
     display: flex;
-    background-color: #d1e7fb;
+    background-color: #e7f3ff;
+    overflow: hidden;
 `
 const Arrow = styled.div`
     width: 50px;
     height: 50px;
-    background-color: #eaf1fc;
+    background-color: #ffffff;
     border-radius: 50%;
     display: flex;
     align-items: center;
     justify-content: center;
+    position: absolute;
+    top:0;
+    bottom:0;
+    left:${props => props.direction === "left" && "10px"};
+    right:${props => props.direction === "right" && "10px"};
+    margin:auto;
+    cursor:pointer;
+    opacity: 0.5;
+    z-index: 2;
+`
+const Wrapper = styled.div`
+    height: 100%;
+    display:flex;
+    transform: transleteX(0vw);
+
+`
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    background-color: #${props=>props.bg};
+`
+const ImgContainer = styled.div`
+    flex:1;
+`
+const Image = styled.img`
+    height: 80%;
+` 
+const InfoContainer = styled.div`
+    flex:1;
+    padding:50px;
+
+`
+const Title = styled.h1`
+    font-size: 70px;
+`
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500px;
+    letter-spacing: 3px;
+`
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color:transparent;
+    cursor: pointer;
 `
 
+
+
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {};
+
   return (
     <Container>
-        <Arrow>
+        
+        <Arrow direction = "left" onClick={()=>handleClick("left")}>
             <ArrowLeftOutlined/>
         </Arrow>
-        <Arrow>
+        <Wrapper>
+
+            <Slide bg="f5fafd">
+            <ImgContainer>
+                <Image src="https://images.pexels.com/photos/1040173/pexels-photo-1040173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            </ImgContainer>
+            <InfoContainer>
+                <Title>SUMMER SALE</Title>
+                <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+                <Button>SHOW NOW</Button>
+            </InfoContainer>
+            </Slide>
+   
+        </Wrapper>
+        <Arrow direction = "right" onClick={()=>handleClick("right")}>
             <ArrowRightOutlined/>
         </Arrow>
     </Container>
