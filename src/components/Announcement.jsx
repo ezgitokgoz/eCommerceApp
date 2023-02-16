ddffbec34989216dd3c0d5e701cf6e67fc47f4cb
Ezgi Tokgoz <ezgitokgoz16@gmail.com>
2023-02-16 15:30:55 +0300

    slider is complete

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components"
 const Container = styled.div`
     height: 30px;
     background-color: #000000;
+    
     color:white;
     display: flex;
     align-items: center;
--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const sliderItems =[
     {
       id:1,
-      img:"https://images.pexels.com/photos/6976749/pexels-photo-6976749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img:"https://images.pexels.com/photos/6976754/pexels-photo-6976754.jpeg?auto=compress&cs=tinysrgb&w=600",
       title:"SUMMER SALE",
       desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg:"f7b47d",
@@ -9,17 +9,26 @@ export const sliderItems =[
 
     {
         id:2,
-        img:"https://images.pexels.com/photos/7037432/pexels-photo-7037432.jpeg?auto=compress&cs=tinysrgb&w=600",
+        img:"https://images.pexels.com/photos/6910334/pexels-photo-6910334.jpeg?auto=compress&cs=tinysrgb&w=600",
         title:"WINTER SALE",
         desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg:"93c9ff",
+        bg:"de7d7d",
     },
 
     {
       id:3,
-      img:"https://images.pexels.com/photos/5444999/pexels-photo-5444999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img:"https://images.pexels.com/photos/9794320/pexels-photo-9794320.jpeg?auto=compress&cs=tinysrgb&w=600",
       title:"POPULAR SALE",
       desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg:"f7e37d",
+      bg:"cfae7e",
     },
+
+    {
+        id:4,
+        img:"https://images.pexels.com/photos/7346634/pexels-photo-7346634.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title:"SPORTSWEAR SALE",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg:"eca7be",
+        
+      },
 ];
